911a718ae7c009d694887b1a96f8303ef5c7fabd
Nikola <nikolaviki@hotmail.com>
2020-05-30 15:54:23 +0200

    Seting todos in local storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
+const LOCAL_STORAGE_KEY = "react-todo-lists-todos";
+
 
 function App() {
   const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (setTodos) {
+      setTodos(storageTodos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(todo) {
     setTodos([todo, ...todos])
